36919e11f79f62be2722704757d46149861757e4
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2025-05-16 13:08:31 -0500

    gitignore update and setup bat

---
+++ setup.bat
@@ -0,0 +1,30 @@
+@echo off
+
+:: Verificar si ya existe el entorno virtual
+if exist venv (
+    echo El entorno virtual 'venv' ya existe.
+) else (
+    echo Creando el entorno virtual 'venv'...
+    python -m venv venv
+)
+
+:: Activar el entorno virtual
+if exist venv\Scripts\activate (
+    echo Activando el entorno virtual...
+    call venv\Scripts\activate
+) else (
+    echo Error: No se pudo encontrar el entorno virtual. Asegúrate de que se creó correctamente.
+    pause
+    exit /b
+)
+
+:: Instalar las dependencias
+echo Instalando las dependencias...
+pip install -r requirements.txt
+
+:: Instalar los navegadores necesarios para Playwright
+echo Instalando los navegadores de Playwright...
+playwright install
+
+echo Setup completo. El entorno virtual está activado.
+pause
